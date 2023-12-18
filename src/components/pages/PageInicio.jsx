2dbbf2c8c4e6c2647eac86f8c2077050d7fe86d5
Jesse Rodrigues <98673779+ujhessie@users.noreply.github.com>
2023-12-18 15:13:26 -0300

    teste

---
+++ src/components/pages/PageInicio.jsx
@@ -0,0 +1,16 @@
+import SecInicio from "../sections/SecInicio"
+import SecSobre from "../sections/SecSobre"
+import SecProjetos from "../sections/SecProjetos"
+
+function PageInicio() {
+    return (
+        <>
+            <SecInicio />
+            <SecSobre />
+            <SecProjetos />
+            
+        </>
+    )   
+}
+
+export default PageInicio
