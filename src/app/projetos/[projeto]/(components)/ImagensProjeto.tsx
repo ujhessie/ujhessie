f4a85b4e4ec672e6abc1bae9d9d46660cb83d9d4
ujhessie <juniorrodrigues4502@gmail.com>
2024-07-02 16:05:03 -0300

    ssd

--- src/app/projetos/[projeto]/(components)/ImagensProjeto.tsx
+++ src/app/projetos/[projeto]/(components)/ImagensProjeto.tsx
@@ -1,26 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 export const ImagensProjeto: React.FC<{ imagens: string[] }> = ({
     imagens,
 }) => {
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            import("scrollreveal").then((module) => {
-                const sr = module.default();
-                sr.reveal(".reveal", {
-                    origin: "bottom",
-                    distance: "20px",
-                    duration: 1000,
-                    reset: true,
-                    interval: 200,
-                });
-            });
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (typeof window !== "undefined") {
+    //         import("scrollreveal").then((module) => {
+    //             const sr = module.default();
+    //             sr.reveal(".reveal", {
+    //                 origin: "bottom",
+    //                 distance: "20px",
+    //                 duration: 1000,
+    //                 reset: true,
+    //                 interval: 200,
+    //             });
+    //         });
+    //     }
+    // }, []);
 
 
     return (
@@ -28,7 +28,8 @@ export const ImagensProjeto: React.FC<{ imagens: string[] }> = ({
             {imagens.map((img, index) => (
                 <img
                     key={index}
-                    className='w-full reveal rounded-xl block mb-4'
+                    className='w-full  rounded-xl block mb-4'
+                    // className='w-full reveal rounded-xl block mb-4'
                     src={img}
                     alt={`Imagem do projeto ${index + 1}`}
                 />
