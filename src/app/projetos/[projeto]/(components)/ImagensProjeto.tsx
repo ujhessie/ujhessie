1246b00f00a348cd5d182fcf5cd151397cad2e0b
ujhessie <juniorrodrigues4502@gmail.com>
2024-07-05 08:36:47 -0300

    asda

--- src/app/projetos/[projeto]/(components)/ImagensProjeto.tsx
+++ src/app/projetos/[projeto]/(components)/ImagensProjeto.tsx
@@ -1,28 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-// import { useEffect } from 'react';
-
 export const ImagensProjeto: React.FC<{ imagens: string[] }> = ({
     imagens,
 }) => {
-
-    // useEffect(() => {
-    //     if (typeof window !== "undefined") {
-    //         import("scrollreveal").then((module) => {
-    //             const sr = module.default();
-    //             sr.reveal(".reveal", {
-    //                 origin: "bottom",
-    //                 distance: "20px",
-    //                 duration: 1000,
-    //                 reset: true,
-    //                 interval: 200,
-    //             });
-    //         });
-    //     }
-    // }, []);
-
-
     return (
         <>
             {imagens.map((img, index) => (
@@ -34,6 +15,6 @@ export const ImagensProjeto: React.FC<{ imagens: string[] }> = ({
                     alt={`Imagem do projeto ${index + 1}`}
                 />
             ))}
-</>
+        </>
     );
 };
