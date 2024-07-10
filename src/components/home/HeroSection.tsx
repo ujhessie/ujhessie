d049b1af329c1a5f3250767aae638de283b712c1
ujhessie <juniorrodrigues4502@gmail.com>
2024-07-10 10:54:48 -0300

    adasd

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,4 +1,4 @@
-import Header from "../ui/Header/Header";
+import {Header} from "../ui/Header/Header";
 import MaxWidthContainer from "../ui/MaxWidthContainer";
 
 export default function HeroSection() {
