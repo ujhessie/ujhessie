69549f8de6830a433c354f9adc9b861e9a139684
ujhessie <juniorrodrigues4502@gmail.com>
2024-07-11 14:28:01 -0300

    asdas

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,19 +1,44 @@
+'use client'
+
 import Link from "next/link";
 import MaxWidthContainer from "../MaxWidthContainer";
-import { Logo } from './components/';
+import { TbMenu } from "react-icons/tb";
+import { Logo } from "./components/";
+import { TfiClose } from "react-icons/tfi";
+import "./header.css";
 
 export const Header = () => {
+    function fecharNav () {
+        document.querySelector(".nav-header")?.classList.remove("nav-ativa")
+    }
+    function abrirNav () {
+        document.querySelector(".nav-header")?.classList.add("nav-ativa")
+    }
+
     return (
-        <MaxWidthContainer className='h-[100px] flex justify-center md:justify-between items-center  bg-preto'>
-           <Logo/>
-            <nav className='py-4 px-8 bg-cinza text-branco rounded-full fonte-corpo  gap-4 hidden lg:flex'>
-                <Link href='/'>Página inicial</Link>
-                <Link href='/'>Projetos</Link>
-                <Link href='/'>Sobre</Link>
-                <Link href='/'>Serviços</Link>
+        <MaxWidthContainer className='h-[100px] z-10 flex justify-between items-center  bg-preto'>
+            <Logo />
+            <nav className=' nav-header   lg:bg-cinza text-branco  fonte-corpo   '>
+                <div className='content  gap-4'>
+                    <div className='div-close md:hidden' onClick={fecharNav} >
+                        <TfiClose className='icon text-3xl' />
+                    </div>
+                    <Link href='/'onClick={fecharNav}>Página inicial</Link>
+                    <Link href='/' onClick={fecharNav}>Projetos</Link>
+                    <Link href='/' onClick={fecharNav}>Sobre</Link>
+                    <Link href='/' onClick={fecharNav}>Serviços</Link>
+                </div>
             </nav>
-            <div className='botoes'>
-                <Link href='/'className='bg-gradient-to-r from-vermelho to-rosa text-white px-8 py-3 rounded-md fonte-titulo font-bold uppercase hidden lg:block'>Contatos</Link>
+            <div className='botoes z-20 flex gap-4'>
+                <Link
+                    href='/'
+                    className='bg-gradient-to-r from-vermelho to-rosa text-white px-8 py-3 rounded-md fonte-titulo font-bold uppercase hidden md:block'
+                >
+                    Contatos
+                </Link>
+                <div className='botao-menu md:hidden relative h-full bg-cinza text-white py-6 rounded-md px-8 flex itens-center justfy-center' >
+                    <TbMenu className='absolute z-10 top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%]  text-3xl' onClick={abrirNav} />
+                </div>
             </div>
         </MaxWidthContainer>
     );
