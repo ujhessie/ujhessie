8943fe461eabcf9868bdd03a46cc924bb518e22e
ujhessie <juniorrodrigues4502@gmail.com>
2024-07-12 14:33:44 -0300

    Botao close adicionado na page de projeto

--- src/app/projetos/[projeto]/page.tsx
+++ src/app/projetos/[projeto]/page.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable jsx-a11y/alt-text */
 "use client";
 
-
 import "./style.css";
 import MaxWidthContainer from "@/components/ui/MaxWidthContainer";
 import { useProjetos } from "@/contexts/ProjetosContexts";
 import { TfiClose } from "react-icons/tfi";
 
 import { Banner, Informacoes, Capa, ImagensProjeto } from "./(components)";
+import Link from "next/link";
 
 const ProjetoPage = ({ params }: { params: { projeto: string } }) => {
     const { projetos } = useProjetos();
@@ -20,10 +20,16 @@ const ProjetoPage = ({ params }: { params: { projeto: string } }) => {
             {/* <Banner banner={projeto?.imagens.imgBanner} /> */}
             <MaxWidthContainer>
                 <section className='content    bg-zinc-950 px-4 py-8 md:p-12 rounded-2xl shadow-sm text-center md:text-start flex flex-col md:grid md:grid-cols-7  gap-8 mb-8 text-white'>
-                <div className='div-close z-10  absolute bg-white rounded-full right-7 p-3 top-4' >
+                    <Link
+                        href='/'
+                        className='div-close z-10  absolute bg-white rounded-full right-7 p-3 top-4'
+                    >
                         <TfiClose className='icon text-2xl text-cinza' />
-                    </div>
-                    <Capa status={projeto?.status} capa={projeto?.imagens.imgCapa ?? ""} />
+                    </Link>
+                    <Capa
+                        status={projeto?.status}
+                        capa={projeto?.imagens.imgCapa ?? ""}
+                    />
                     <Informacoes
                         titulo={projeto?.titulo ?? ""}
                         descricao={projeto?.desc ?? ""}
